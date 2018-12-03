708377ee95d8a60402ff90ba7228fb32161e5f4b
Romana <romananasrinmimi@gmail.com>
2018-12-03 12:07:20 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print hello world
+            Console.WriteLine("Hello World.");
+
+            //take user input and show them
+            Console.Write("enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("enter your phone no: ");
+            double phoneNo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n");
+            Console.WriteLine("User Information: ");
+            Console.Write("Name: "+name+" ");
+            Console.WriteLine("Phone No: "+phoneNo);
+            Console.ReadKey();
+
+
+        }
+    }
+}
